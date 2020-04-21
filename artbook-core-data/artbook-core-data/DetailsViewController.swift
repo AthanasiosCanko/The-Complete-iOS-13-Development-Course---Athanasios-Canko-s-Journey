b4d4069037d2aff55cc5c8c5ba02be1139fbbf2f
Athanasios Canko <cankoathanasios@gmail.com>
2020-04-21 18:21:54 +0200

    Adding keyboard hiding functions and image picker for artbook-core-data

--- artbook-core-data/artbook-core-data/DetailsViewController.swift
+++ artbook-core-data/artbook-core-data/DetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameInput: UITextField!
@@ -17,8 +17,44 @@ class DetailsViewController: UIViewController {
     
     @IBAction func saveButton(_ sender: Any) {
     }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+//        view.addGestureRecognizer(tap)
+        
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(imageTap)
+
+//        self.nameInput.delegate = self
+//        self.artistInput.delegate = self
+//        self.yearInput.delegate = self
     }
+    
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        view.endEditing(true)
+//    }
+    
+    @objc func pickImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+//    @objc func hideKeyboard() {
+//        view.endEditing(true)
+//    }
 }
