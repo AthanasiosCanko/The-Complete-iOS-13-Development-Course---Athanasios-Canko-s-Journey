c35d5b2823a70445ec36b502229dc79bee36bd6b
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-07 19:41:37 +0200

    Adding new project: face-recognition-project

---
+++ face-recognition-project/face-recognition-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  face-recognition-project
+//
+//  Created by Athanasios Canko on 7.5.20.
+//  Copyright © 2020 Athanasios Canko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
