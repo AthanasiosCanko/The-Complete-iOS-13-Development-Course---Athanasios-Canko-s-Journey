21b15f07fc1178556ad700ce02e6d113654391ad
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-12 14:27:18 +0200

    Adding new project: currency-converter

---
+++ currency-converter/currency-converter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  currency-converter
+//
+//  Created by Athanasios Canko on 12.5.20.
+//  Copyright © 2020 Athanasios Canko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    @IBAction func getRatesClicked(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
