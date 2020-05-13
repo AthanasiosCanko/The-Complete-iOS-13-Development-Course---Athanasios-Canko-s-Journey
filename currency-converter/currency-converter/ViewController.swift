ed392433d8d42291f5031cd99f224882b518fc46
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-13 16:39:16 +0200

    Adding requests and session for currency-converter

--- currency-converter/currency-converter/ViewController.swift
+++ currency-converter/currency-converter/ViewController.swift
@@ -19,11 +19,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var tryLabel: UILabel!
     
     @IBAction func getRatesClicked(_ sender: Any) {
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=5c3667174a08de0f3ae53e6091da1930&format=1")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }
+            else {
+                if data != nil {
+                    //
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
