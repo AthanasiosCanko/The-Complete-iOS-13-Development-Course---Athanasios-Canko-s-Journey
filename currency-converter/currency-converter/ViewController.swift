f860bd5dadf443c01696eebea68607e7ce8a0054
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-13 16:40:15 +0200

    Adding comments for currency-converter

--- currency-converter/currency-converter/ViewController.swift
+++ currency-converter/currency-converter/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tryLabel: UILabel!
     
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 1. Request & Session
+        // 2. Response & Data
+        // 3. Parsing
+        
+        // 1.
         let url = URL(string: "http://data.fixer.io/api/latest?access_key=5c3667174a08de0f3ae53e6091da1930&format=1")
         let session = URLSession.shared
         
@@ -30,6 +36,8 @@ class ViewController: UIViewController {
                 self.present(alert, animated: true, completion: nil)
             }
             else {
+                
+                // 2.
                 if data != nil {
                     //
                 }
