90573ed8b24112e9ffa22cc5e2cfa32e5cef6fb7
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-19 18:42:46 +0200

    ADding new pod installations

--- insta-clone-firebase/insta-clone-firebase/ViewController.swift
+++ insta-clone-firebase/insta-clone-firebase/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
