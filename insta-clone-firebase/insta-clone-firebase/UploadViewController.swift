10610e3559bfb6c71344078767804d4d3a88387f
Athanasios Canko <cankoathanasios@gmail.com>
2020-05-21 22:20:01 +0200

    Adding upload VC details

--- insta-clone-firebase/insta-clone-firebase/UploadViewController.swift
+++ insta-clone-firebase/insta-clone-firebase/UploadViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentInput: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commentInput.delegate = self
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func chooseImage() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            imageView.image = image
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
