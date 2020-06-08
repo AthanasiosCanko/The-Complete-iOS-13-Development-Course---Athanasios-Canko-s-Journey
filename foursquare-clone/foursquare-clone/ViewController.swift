0c92ce8e3f799e6cf363903a39be20f80f9bf046
Athanasios Canko <cankoathanasios@gmail.com>
2020-06-08 16:47:23 +0200

    Adding foursquare-clone

---
+++ foursquare-clone/foursquare-clone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  foursquare-clone
+//
+//  Created by Athanasios Canko on 8.6.20.
+//  Copyright © 2020 Athanasios Canko. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
