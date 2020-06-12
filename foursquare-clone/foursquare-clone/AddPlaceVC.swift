871d1607443e24fd8d528c6557f0b39aa9fdb885
Athanasios Canko <cankoathanasios@gmail.com>
2020-06-12 14:10:41 +0200

    Adding AddPlaceVC for foursquare-clone

---
+++ foursquare-clone/foursquare-clone/AddPlaceVC.swift
@@ -0,0 +1,24 @@
+//
+//  AddPlaceVC.swift
+//  foursquare-clone
+//
+//  Created by Athanasios Canko on 12.6.20.
+//  Copyright © 2020 Athanasios Canko. All rights reserved.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    
+    @IBOutlet weak var placeNameText: UITextField!
+    @IBOutlet weak var placeTypeText: UITextField!
+    @IBOutlet weak var placeDescriptionText: UITextField!
+    @IBOutlet weak var placeImageView: UIImageView!
+    @IBAction func nextButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- foursquare-clone/foursquare-clone/PlacesViewController.swift
+++ foursquare-clone/foursquare-clone/PlacesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesViewController: UIViewController {
 
@@ -14,9 +15,22 @@ class PlacesViewController: UIViewController {
         super.viewDidLoad()
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logOut))
     }
     
     @objc func addButtonClicked() {
         //
+    }
+    
+    @objc func logOut() {
+        PFUser.logOutInBackground { (error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else {
+                self.performSegue(withIdentifier: "logOut", sender: nil)
+            }
+        }
     }
 }
