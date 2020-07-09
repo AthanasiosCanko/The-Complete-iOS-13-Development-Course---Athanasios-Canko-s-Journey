fad6f09285cbbd653476bd503872600f413b2965
Athanasios Canko <cankoathanasios@gmail.com>
2020-07-09 19:25:19 +0200

    Adding guard let concept and switches for advanced-swift-projject

--- advanced-swift-project/advanced-swift-project/main.swift
+++ advanced-swift-project/advanced-swift-project/main.swift
@@ -28,11 +28,77 @@ structJames.age = 51
 //
 //print(classJames.age)
 //print(structJames.age)
+//
+//print(classJames.age)
+//classJames.happyBirthday()
+//print(classJames.age)
+//
+//print(structJames.age)
+//structJames.happyBirthday()
+//print(structJames.age)
 
-print(classJames.age)
-classJames.happyBirthday()
-print(classJames.age)
+// TUPLE
+let myTuple = (1, 3)
+print(myTuple.0)
 
-print(structJames.age)
-structJames.happyBirthday()
-print(structJames.age)
+var myTuple2 = (1, 3, 5)
+myTuple2.2 = 10
+print(myTuple2.2)
+
+let myTuple3 = ("Atil", 100)
+let myTuple4 = (10, [10, 20, 30])
+print(myTuple4.1[1])
+
+let predefinedTuple: (String, String)
+predefinedTuple.0 = "Atil"
+predefinedTuple.1 = "Sam"
+
+let newTuple = (name: "Athanasios", age: 24)
+print(newTuple.name)
+print(newTuple.age)
+
+// Guard Let vs If Let
+// Guard -> Negative && Confident
+// If -> Positive
+
+let myNumber = "5"
+func convertToIntegerGuard(stringInput: String) -> Int {
+    guard let myInteger = Int(stringInput) else {
+        return 0
+    }
+    return myInteger
+}
+
+func convertToIntegerIf(stringInput: String) -> Int {
+    if let myInteger = Int(stringInput) {
+        return myInteger
+    }
+    return 0
+}
+
+print(convertToIntegerIf(stringInput: myNumber))
+print(convertToIntegerGuard(stringInput: myNumber))
+
+// Switch
+let myNum = 11
+let myRemainder = myNum % 2
+
+switch myRemainder {
+case 0:
+    print("0")
+case 1:
+    print("1")
+case 2:
+    print("2")
+case 3:
+    print("3")
+default:
+    print("error")
+}
+
+switch myRemainder {
+case 1...3:
+    print("It's 1-3")
+default:
+    print("None")
+}
