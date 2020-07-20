5fd56e05baf3390bbfc6f54af3fa7878ac72167d
Athanasios Canko <cankoathanasios@gmail.com>
2020-07-20 16:37:08 +0200

    Completing machine-learning-image-recognition

--- machine-learning-image-recognition/machine-learning-image-recognition/ViewController.swift
+++ machine-learning-image-recognition/machine-learning-image-recognition/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var chosenImage = CIImage()
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
@@ -24,15 +28,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = image
             dismiss(animated: true, completion: nil)
             
+            if let ciImage = CIImage(image: imageView.image!) {
+                chosenImage = ciImage
+            }
             
-            
-            
-            
-            
+            recognizeImage(image: chosenImage)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func recognizeImage(image: CIImage) {
+        
+        resultLabel.text = "Finding..."
+        // 1) Request
+        // 2) Handler
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+            let request = VNCoreMLRequest(model: model) { (vnrequest, error) in
+                if error == nil {
+                    if let results = vnrequest.results as? [VNClassificationObservation] {
+                        if results.count > 0 {
+                            let topResult = results.first
+                            
+                            DispatchQueue.main.async {
+                                //
+                                let confidence = (topResult?.confidence ?? 0) * 100
+                                let rounded = Int(confidence)
+                                self.resultLabel.text = "Confidence: \(rounded) for: \(topResult!.identifier)"
+                            }
+                        }
+                    }
+                }
+                else {
+                    print(error?.localizedDescription)
+                }
+            }
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([request])
+                }
+                catch {
+                    print("Error")
+                }
+            }
+        }
+        
     }
 }
