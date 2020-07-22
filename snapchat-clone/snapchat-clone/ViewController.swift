806f302119bd2afe8896a6e1c3e57b881737cbcc
Athanasios Canko <cankoathanasios@gmail.com>
2020-07-22 16:17:28 +0200

    Initializing and configuring Firebase for snapchat-clone

---
+++ snapchat-clone/snapchat-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapchat-clone
+//
+//  Created by Athanasios Canko on 22.7.20.
+//  Copyright © 2020 Athanasios Canko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
