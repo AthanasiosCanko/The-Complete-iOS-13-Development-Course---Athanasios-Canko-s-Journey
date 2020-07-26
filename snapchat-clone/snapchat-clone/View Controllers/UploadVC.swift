d915b802b27a6bcb2a7b9e4b01de7cce2e3e5671
Athanasios Canko <cankoathanasios@gmail.com>
2020-07-26 18:08:47 +0200

    Adding image upload capability for snapchat-clone

--- snapchat-clone/snapchat-clone/View Controllers/UploadVC.swift
+++ snapchat-clone/snapchat-clone/View Controllers/UploadVC.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func uploadClicked(_ sender: Any) {
-        //
+        let storage = Storage.storage()
+        let imageReference = storage.reference().child("media/").child("\(UUID()).jpg")
+        
+        if let imageData = imageView.image?.jpegData(compressionQuality: 0.8) as? Data {
+            imageReference.putData(imageData, metadata: nil) { (metadata, error) in
+                if error == nil {
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            let firestore = Firestore.firestore()
+                            
+                            let snapDictionary = [
+                                "imageUrl": url!.absoluteString,
+                                "snapOwner": UserSingleton.sharedUserInfo.username,
+                                "date": FieldValue.serverTimestamp()
+                            ] as [String: Any]
+                            
+                            firestore.collection("Snaps").addDocument(data: snapDictionary) { (error) in
+                                if error == nil {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.imageView.image = UIImage(systemName: "photo")
+                                }
+                                else {
+                                    print(error?.localizedDescription)
+                                }
+                            }
+                        }
+                        else {
+                            print(error?.localizedDescription)
+                        }
+                    }
+                }
+                else {
+                    print(error?.localizedDescription)
+                }
+            }
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
